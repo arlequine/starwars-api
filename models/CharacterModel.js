87791cdf175646e22423fc0ec7d5ec3fe6c41f28
Alfredo hernandez <arlequine666.ah@gmail.com>
2024-12-05 21:45:55 -0600

    fix(model):add team model

--- models/CharacterModel.js
+++ models/CharacterModel.js
@@ -4,6 +4,9 @@ const characterSchema = new mongoose.Schema({
     name: { type: String, required: true },
     movie: { type: String, required: true },
     ship: { type: String, required: true },
+    team: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Character', characterSchema);
